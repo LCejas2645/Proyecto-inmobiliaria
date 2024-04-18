ffb70c64f3aefe13ff44f266a6557547d24afc62
LCejas2645 <101837605+LCejas2645@users.noreply.github.com>
2024-04-18 00:42:31 -0300

    Pago Creacion de carpetas

---
+++ ABM-inmobiliaria/Controllers/PagoController.cs
@@ -0,0 +1,11 @@
+using ABM_inmobiliaria.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+
+
+namespace ABM_inmobiliaria.Controllers{
+    public class PagoController : Controller{
+
+    }
+}
---
+++ ABM-inmobiliaria/Models/RepositorioPago.cs
@@ -0,0 +1,12 @@
+using MySql.Data.MySqlClient;
+
+
+namespace ABM_inmobiliaria.Models{
+    public class RepositorioPago{
+        public RepositorioPago()
+        {
+        }
+
+        readonly string ConnectionString = "Server=localhost;Database=Inmobiliaria;User=root;Password=;";
+    }
+}
